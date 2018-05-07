fdaa8456983765dbe1824b630bf5e2b9f1184666
CuryT1993 <curytravez_t.v@hotmail.com>
2018-05-07 08:10:10 -0500

    correcion2
    otracadena

--- php/conexion.php
+++ php/conexion.php
@@ -5,13 +5,20 @@ $password="curytravez";
 $db="registroDB";
 $con = new mysqli($host,$user,$password,$db);
 */
-$host="curytravez.postgres.database.azure.com";
-$user="curytravez@curytravez";
-$port=5432;
-$password="Atahualpacury1993";
-$dbname="registroDB";
-$sslmode=required;
-//$con = new mysqli($host,$user,$password,$db);
+
+
+ // Initialize connection variables.
+    $host = "curytravez.postgres.database.azure.com";
+    $database = "registroDB";
+    $user = "curytravez@curytravez";
+    $password = "Atahualpacury1993";
+
+    // Initialize connection object.
+    $connection = pg_connect("host=$host dbname=$database user=$user password=$password")
+                or die("Failed to create connection to database: ". pg_last_error(). ".<br/>");
+
+    print "Successfully created connection to database. <br/>";
+
 
 
 ?>
