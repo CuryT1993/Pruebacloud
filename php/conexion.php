3188d55b986e34b4c81efb4c3cddc029241bc486
CuryT1993 <curytravez_t.v@hotmail.com>
2018-05-07 20:48:52 -0500

    Otraconexion
    Prueba

--- php/conexion.php
+++ php/conexion.php
@@ -14,6 +14,22 @@ $con = new mysqli($host,$user,$password,$db);
     //$password = "Atahualpacury1993";
 
     // Initialize connection object.
-self::$conexion = new PDO("pgsql:host=curytravez.postgres.database.azure.com" . self::$dbHost . ";port=5432;dbname=registroDB" . self::$dbName . ";user=curytravez@curytravez" . self::$dbUsername . ";password=Atahualpacury1993" . self::$dbUserPassword . ";sslmode=require");
+//self::$conexion = new PDO("pgsql:host=curytravez.postgres.database.azure.com" . self::$dbHost . ";port=5432;dbname=registroDB" . self::$dbName . ";user=curytravez@curytravez" . self::$dbUsername . ";password=Atahualpacury1993" . self::$dbUserPassword . ";sslmode=require");
+
+$host="curytravez.postgres.database.azure.com";
+$port="5432";
+$user="curytravez@curytravez";
+$pass="Atahualpacury1993";
+$dbname="registroDB";
+
+$connect = pg_connect("host=$host, port=$port, user=$user, 
+pass=$pass, dbname=$dbname");
+
+if(!$connect)
+echo "<p><i>No me conecte</i></p>";
+else
+echo "<p><i>Me conecte</i></p>";
+
+pg_close($connect)
 
 ?>
