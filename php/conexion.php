795034b4ae71e24bb9b104ffc062da43089d5935
CuryT1993 <curytravez_t.v@hotmail.com>
2018-05-07 23:23:25 -0500

    probndo otro
    conexion

--- php/conexion.php
+++ php/conexion.php
@@ -14,7 +14,42 @@ $con = new mysqli($host,$user,$password,$db);
     //$password = "Atahualpacury1993";
 
     // Initialize connection object.
-self::$conexion = new PDO("pgsql:host=curytravez.postgres.database.azure.com" . self::$dbHost . ";port=5432;dbname=registroDB" . self::$dbName . ";user=curytravez@curytravez" . self::$dbUsername . ";password=Atahualpacury1993" . self::$dbUserPassword . ";sslmode=require");
+//self::$conexion = new PDO("pgsql:host=curytravez.postgres.database.azure.com" . self::$dbHost . ";port=5432;dbname=registroDB" . self::$dbName . ";user=curytravez@curytravez" . self::$dbUsername . ";password=Atahualpacury1993" . self::$dbUserPassword . ";sslmode=require");
+
+
+ 
+ //dbname
+ private $dbname = "registroDB";
+ //host
+ private $host = "curytravez.postgres.database.azure.com";
+ //user database
+ private $user = "curytravez@curytravez";
+ //password user
+ private $pass = 'Atahualpacury1993';
+ //port
+ private $port = 5432;
+    //instance
+ private $dbh;
+ 
+ //connect with postgresql and pdo
+ public function __construct()
+ {
+     try {
+         $this->dbh = parent::__construct("pgsql:host=$this->host;port=$this->port;dbname=$this->dbname;user=$this->user;password=$this->pass");
+     }
+        catch(PDOException $e)
+        {
+         echo  $e->getMessage();
+     }
+ 
+ }
+ 
+ //función para cerrar una conexión pdo
+ public function close()
+ {
+    	$this->dbh = null;
+ }
+ 
 
 
 
