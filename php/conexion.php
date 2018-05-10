273da9744ab4ca89986743e2d69a2309f793f2c9
CuryT1993 <curytravez_t.v@hotmail.com>
2018-05-09 23:03:56 -0500

    my
    my

--- php/conexion.php
+++ php/conexion.php
@@ -24,6 +24,21 @@ $con = new mysqli($host,$user,$password,$db);
 */
 
 
-$con=mysqli_init(); [mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL);] mysqli_real_connect($con, "curytravez.postgres.database.azure.com","curytravez@curytravez", "Atahualpacury1993", "registrodb", "3306");
+//$con=mysqli_init(); [mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL);] mysqli_real_connect($con, "curytravez.postgres.database.azure.com","curytravez@curytravez", "Atahualpacury1993", "registrodb", "3306");
+
+$host = 'curytravez.postgres.database.azure.com';
+$username = 'curytravez@curytravez';
+$password = 'Atahualpacury1993';
+$db_name = 'registrodb';
+
+//Establishes the connection
+$conn = mysqli_init();
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+//Close the connection
+mysqli_close($conn);
 
 ?>
