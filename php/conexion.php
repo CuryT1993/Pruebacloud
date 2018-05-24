9db7d80cabd675be54097448ad552e11403c3cd6
CuryT1993 <curytravez_t.v@hotmail.com>
2018-05-23 22:30:52 -0500

    f
    f

--- php/conexion.php
+++ php/conexion.php
@@ -17,10 +17,14 @@ $con = new mysqli($host,$user,$password,$db);
 //self::$conexion = new PDO("pgsql:host=curytravez.postgres.database.azure.com" . self::$dbHost . ";port=5432;dbname=registroDB" . self::$dbName . ";user=curytravez@curytravez" . self::$dbUsername . ";password=Atahualpacury1993" . self::$dbUserPassword . ";sslmode=require");
 
 
-
+class Database
+{
+    public static function Conectar()
+    {
         $pdo = new PDO('mysql:host=curyservidor.mysql.database.azure.com;dbname=myapp;charset=utf8', 'curytravez@curyservidor', 'Atahualpacury1993');
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
         return $pdo;
-
+    }
+}
 
 ?>
